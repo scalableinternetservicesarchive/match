f97afc469477f7d35b0792a6e4e97d7a0fb7ac08
Siddharth Joshi <sjoshi804@gmail.com>
2019-10-30 21:31:57 -0700

    authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
+  devise_for :users 
+  authenticated :user do
+    root to: 'pages#home', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
+  get '/users/sign_out' => 'devise/sessions#destroy'
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
