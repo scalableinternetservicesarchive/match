edb7663b5aeb655fd371b3a12ea564241fccf674
Siddharth Joshi <sjoshi804@gmail.com>
2019-11-01 10:20:29 -0700

    profile page working

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
