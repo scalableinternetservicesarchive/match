929f0824b1df7539380a6394da037b1e02cf08cf
Siddharth Joshi <sjoshi804@gmail.com>
2019-11-01 11:20:15 -0700

    sign in protected all pages except landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, :except => [:home]
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users 
-  authenticated :user do
-    root to: 'pages#home', as: :authenticated_root
-  end
-  root to: redirect('/users/sign_in')
+  root to: 'pages#home'
   get '/users/sign_out' => 'devise/sessions#destroy'
   get 'pages/home'
   get 'pages/about'
