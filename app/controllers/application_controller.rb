331679690f750cff196c461b2a66077bfdc96ed4
Siddharth Joshi <sjoshi804@gmail.com>
2019-11-03 01:03:27 -0700

    sign up bug working, allowed devise to accept other fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :first_name, :last_name, :phone) }
+      end
+  end
+  
