b18ff41a83862ade133fba47a2e7b48733c887d3
Siddharth Joshi <sjoshi804@gmail.com>
2019-11-05 08:50:12 -0800

    added functionality to create a post

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,5 +1,20 @@
 class MatchController < ApplicationController
-    def post
+    def new_post_form
+    end
+    def new_post
+        @game = Game.new()
+        @game.interest = params[:interest]
+        @game.venue = params[:venue]
+        @game.date = params[:date]
+        @game.start_time = params[:start_time]
+        @game.end_time = params[:end_time]
+        @game.description = params[:description]
+        #TODO: Need to link to organizer - first define has many relation in game model
+        if @game.save
+            redirect_to pages_home_url => 'pages#home', notice: "Post successfully created" and return
+        else
+            render :new
+        end
     end
     def search
     end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/home'
-  resources :users, only: [:show]
+  get 'post/new' => 'match#new_post_form'
+  post 'post/new' => 'match#new_post'
+  resource :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
