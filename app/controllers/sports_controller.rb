be4ce55dfaf3d4bf573dfb8e0df232bc05e9fce1
arjun-kallapur <arjun.kallapur@gmail.com>
2019-11-13 23:46:29 -0800

    Initial commit of sport function, feed implementation and testing to be done

---
+++ app/controllers/sports_controller.rb
@@ -0,0 +1,16 @@
+class SportsController < ApplicationController
+    before_action :authenticate_user!
+    def add_sport_form
+    end
+    def add_sport
+        @sport = Sport.new()
+        @sport.user_id = current_user.id
+        @sport.name = params[:name]
+        if @sport.save
+            flash[:notice] = "Sport successfully added!"
+            redirect_to pages_home_url => 'pages#home'
+        else
+            render :new
+        end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_054117) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "timeslots", force: :cascade do |t|
@@ -62,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_054117) do
     t.string "first_name"
     t.string "last_name"
     t.integer "phone"
+    t.string "interest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
