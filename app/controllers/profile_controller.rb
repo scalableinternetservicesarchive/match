88da24b9e13406363c355ede13c278356f472285
Siddharth Joshi <sjoshi804@gmail.com>
2019-11-27 21:33:18 -0800

    comment to explain caching choices

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -8,7 +8,7 @@ class ProfileController < ApplicationController
         accepted_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:false).select("game_id")
         created_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:true).select("game_id")
         if stale?(etag: accepted_game_ids, etag: created_game_ids)
-          @accepted_games = Game.where('id IN (?)',accepted_game_ids)
+          @accepted_games = Game.where('id IN (?)',accepted_game_ids) #cause there queries are expensive-ish
           @created_games = Game.where('id IN (?)',created_game_ids)
         end
     else
