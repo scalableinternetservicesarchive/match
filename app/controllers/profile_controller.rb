5c8a37c858f619574adaae0bc6f73d09e5adac92
Siddharth Joshi <sjoshi804@gmail.com>
2019-11-27 20:01:07 -0800

    avoid expensive query of pulling in game if ids haven't changed

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,14 +2,16 @@ class ProfileController < ApplicationController
   #Map profile/username to username's profile page
   def show
     if current_user
-      @user = User.find_by(username: params[:username])
-      # User is signed in, so get their timeslots
-      @timeslots = Timeslot.where(user_id: @user.id)
-
-      accepted_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:false).select("game_id")
-      @accepted_games = Game.where('id IN (?)',accepted_game_ids)
-      created_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:true).select("game_id")
-      @created_games = Game.where('id IN (?)',created_game_ids)
+        @user = User.find_by(username: params[:username])
+        # User is signed in, so get their timeslots
+        @timeslots = Timeslot.where(user_id: @user.id)
+        accepted_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:false).select("game_id")
+        created_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:true).select("game_id")
+        if stale?(etag: accepted_game_ids, last_modified: accepted_game_ids.updated_at, etag: created_game_ids, last_modified: created_game_ids.updated_at)
+          @accepted_games = Game.where('id IN (?)',accepted_game_ids)
+          @created_games = Game.where('id IN (?)',created_game_ids)
+        end
+      end
     else
       # User is not signed in
   end
