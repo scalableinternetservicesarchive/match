0754ad63768fe8fe862b001dbb1c488edbd21ef1
Siddharth Joshi <sjoshi804@gmail.com>
2019-11-27 21:32:51 -0800

    if stale also working

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,14 +7,13 @@ class ProfileController < ApplicationController
         @timeslots = Timeslot.where(user_id: @user.id)
         accepted_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:false).select("game_id")
         created_game_ids = PlayerGameMapping.where(user_id:@user.id,is_organizer:true).select("game_id")
-        if stale?(etag: accepted_game_ids, last_modified: accepted_game_ids.updated_at, etag: created_game_ids, last_modified: created_game_ids.updated_at)
+        if stale?(etag: accepted_game_ids, etag: created_game_ids)
           @accepted_games = Game.where('id IN (?)',accepted_game_ids)
           @created_games = Game.where('id IN (?)',created_game_ids)
         end
-      end
     else
       # User is not signed in
-  end
+    end
 
   def form_submission
     redirect_to '/schedule/add'
