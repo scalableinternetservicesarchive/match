90cb335f3508316057ad71cf9a9748443faeda78
Siddharth Joshi <sjoshi804@gmail.com>
2019-12-02 21:18:00 -0800

    cleaned up

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,33 +2,20 @@ class ScheduleController < ApplicationController
     before_action :authenticate_user!
     
     def add_timeslot_form
-        puts "ur mum"
+        puts "launching add timeslot form"
     end
     
     def add_timeslot
         @empty_responses = []
-
-        
         @timeslot = Timeslot.new()
         @timeslot.user_id = current_user.id
         @timeslot.time = params[:time]        
         @timeslot.day = params[:day]
-
-
         #TODO: Need to link to organizer - first define has many relation in game model
         if @timeslot.save
-            puts " here2"
-
-            
+            puts "adding timeslot"
             flash[:notice] = "Timeslot successfully created!"
-            
             render :js => "window.location = '/profile/#{current_user.username}'"
-            
-                
-            
-
-        
-        
         else
             redirect_to 'add'
         end
